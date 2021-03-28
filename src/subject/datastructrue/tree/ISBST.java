19eaca63804112f74c230ce35e22f20a1ae60bbb
JarvisHsu <xu3256622923@gmail.com>
2021-03-28 21:29:30 +0800

    360笔试第二题

--- src/subject/datastructrue/tree/ISBST.java
+++ src/subject/datastructrue/tree/ISBST.java
@@ -6,17 +6,18 @@ package subject.datastructrue.tree;
  */
 public class ISBST {
     public static void main(String[] args) {
-
-        //System.out.println(isBST());
+        BTree bTree = new BTree(new int[]{3, 2, 5, 8, 4, 7, 6, 9, 0, 1});
+        System.out.println(new ISBST().isBST(new TreeNode()));
     }
-    public boolean isBST(TreeNode root){
-        if(root==null){
+
+    public boolean isBST(TreeNode root) {
+        if (root == null) {
             return true;
         }
-        if(root.left!=null&&root.left.val>= root.val){
+        if (root.left != null && root.left.val >= root.val) {
             return false;
         }
-        if(root.right!=null&&root.right.val<= root.val){
+        if (root.right != null && root.right.val <= root.val) {
             return false;
         }
         return isBST(root.left) && isBST(root.right);
